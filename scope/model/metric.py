bdd1a6ec5ec9fd5ead3bea40b505503cdd11ad75
Julian Klug <tensu.wave@gmail.com>
2021-03-01 08:21:09 +0100

    Added ROC AUC

--- scope/model/metric.py
+++ scope/model/metric.py
@@ -21,9 +21,10 @@ def top_k_acc(output, target, k=3):
     return correct / len(target)
 
 
-def roc_auc(label_gt, label_pred):
-    y_true = np.array(label_gt).flatten()
-    y_scores = np.array(label_pred).flatten()
+def roc_auc(label_pred, label_gt):
+    label_pred = torch.argmax(label_pred, dim=1)
+    y_true = label_gt.detach().numpy().flatten()
+    y_scores = label_pred.detach().numpy().flatten()
 
     fpr, tpr, roc_thresholds = roc_curve(y_true, y_scores)
     roc_auc_score = auc(fpr, tpr)
