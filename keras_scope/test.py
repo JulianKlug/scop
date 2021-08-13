57cc6a4ce56304a05fe1a315e8b7a37fd98fbfee
Julian Klug <tensu.wave@gmail.com>
2021-08-13 14:26:58 +0200

    Updated keras_metrics

--- keras_scope/test.py
+++ keras_scope/test.py
@@ -1,25 +1,8 @@
-from tensorflow.keras import backend as K
 import tensorflow as tf
 from keras_scope.datasets.gsd_outcome_dataset import get_gsd_outcome_dataset
+from keras_scope.metrics import f1_m
 from keras_scope.model import get_model
 
-
-def recall_m(y_true, y_pred):
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    recall = true_positives / (possible_positives + K.epsilon())
-    return recall
-
-def precision_m(y_true, y_pred):
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    return precision
-
-def f1_m(y_true, y_pred):
-    precision = precision_m(y_true, y_pred)
-    recall = recall_m(y_true, y_pred)
-    return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 def test():
     model_path = '/Users/jk1/Downloads/3d_image_classification.h5'
@@ -47,6 +30,7 @@ def test():
     result = model.evaluate(test_dataset)
 
     print(dict(zip(model.metrics_names, result)))
+
 
 if __name__ == '__main__':
     test()
