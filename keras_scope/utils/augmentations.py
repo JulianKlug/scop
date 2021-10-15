6ac8208a3850c5dded256afbf4379f1ae6e3611d
Julian Klug <tensu.wave@gmail.com>
2021-10-15 12:22:20 +0200

    Avoid numpy calls in augmentations

--- keras_scope/utils/augmentations.py
+++ keras_scope/utils/augmentations.py
@@ -1,58 +1,79 @@
 import random
+
+import numpy as np
 import tensorflow as tf
 from skimage.exposure import equalize_hist
 from monai.transforms import RandGaussianNoise, RandStdShiftIntensity, RandScaleIntensity, RandAdjustContrast, \
     RandGaussianSharpen, RandHistogramShift, RandAffine, Rand3DElastic
 
+
 def solarize(image: tf.Tensor, threshold: int = 0.5) -> tf.Tensor:
-  """Solarize the input image(s)."""
-  # For each pixel in the image, select the pixel
-  # if the value is less than the threshold.
-  # Otherwise, subtract max from the pixel.
-  return tf.where(image < threshold*tf.reduce_max(image), image, tf.reduce_max(image) - image).numpy()
+    """Solarize the input image(s)."""
+    # For each pixel in the image, select the pixel
+    # if the value is less than the threshold.
+    # Otherwise, subtract max from the pixel.
+    return tf.where(image < threshold * tf.reduce_max(image), image, tf.reduce_max(image) - image).numpy()
+
 
 def equalize(volume):
     return equalize_hist(volume)
+
 
 def elastic(volume):
     transformer = Rand3DElastic(sigma_range=(5, 7), magnitude_range=(100, 150), prob=1, padding_mode='zeros')
     return transformer(volume)
 
+
 def rotate(volume):
     transformer = RandAffine(prob=1, rotate_range=((30, -30), (10, -10), (10, -10)), padding_mode='zeros')
-    return transformer(volume).numpy()
+    transformed_volume = transformer(volume)
+    return safe_numpy_conversion(transformed_volume)
+
 
 def shear(volume):
     transformer = RandAffine(prob=1, shear_range=((1, -1), (1, -1), (1, -1)), padding_mode='zeros')
-    return transformer(volume).numpy()
+    transformed_volume = transformer(volume)
+    return safe_numpy_conversion(transformed_volume)
+
 
 def translate(volume):
     transformer = RandAffine(prob=1, translate_range=((10, -10), (10, -10), (10, -10)), padding_mode='zeros')
-    return transformer(volume).numpy()
+    transformed_volume = transformer(volume)
+    return safe_numpy_conversion(transformed_volume)
+
 
 def scale(volume):
     transformer = RandAffine(prob=1, scale_range=(0.9, 1.1), padding_mode='zeros')
-    return transformer(volume).numpy()
+    transformed_volume = transformer(volume)
+    return safe_numpy_conversion(transformed_volume)
+
 
 def noise(volume):
     transformer = RandGaussianNoise()
-    return transformer(volume).numpy()
+    transformed_volume = transformer(volume)
+    return safe_numpy_conversion(transformed_volume)
+
 
 def shiftIntensity(volume):
     transformer = RandStdShiftIntensity(prob=1, factors=(5, 10))
     return transformer(volume)
 
+
 def scaleIntensity(volume):
     transformer = RandScaleIntensity(prob=1, factors=(5, 10))
-    return transformer(volume).numpy()
+    transformed_volume = transformer(volume)
+    return safe_numpy_conversion(transformed_volume)
+
 
 def adjustContrast(volume):
     transformer = RandAdjustContrast(prob=1)
     return transformer(volume)
 
+
 def sharpen(volume):
     transformer = RandGaussianSharpen(prob=1)
     return transformer(volume)
+
 
 def histogramShift(volume):
     transformer = RandHistogramShift(num_control_points=10, prob=1)
@@ -62,11 +83,20 @@ def histogramShift(volume):
 def identity(volume):
     return volume
 
+
 def to_channels_first(tensor):
     return tf.experimental.numpy.moveaxis(tensor, 3, 0)
 
+
 def to_channels_last(tensor):
     return tf.experimental.numpy.moveaxis(tensor, 0, 3)
+
+
+def safe_numpy_conversion(object):
+    if type(object) is np.ndarray:
+        return object
+    else:
+        return object.numpy()
 
 
 def augment_list():
@@ -87,6 +117,7 @@ def augment_list():
 
     return l
 
+
 '''
 Transforms in original paper: [
     ’Identity’, ’AutoContrast’, ’Equalize’,
@@ -94,6 +125,7 @@ Transforms in original paper: [
     ’Contrast’, ’Brightness’, ’Sharpness’,
     ’ShearX’, ’ShearY’, ’TranslateX’, ’TranslateY’]
 '''
+
 
 class RandAugment3D:
     def __init__(self, n):
