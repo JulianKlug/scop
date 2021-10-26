9c1e36fc620082bc6e34b86783cdd25afe135cae
Julian Klug <tensu.wave@gmail.com>
2021-10-26 15:56:25 +0200

    Only import efficient net if needed

--- scope/models/get_model.py
+++ scope/models/get_model.py
@@ -1,6 +1,5 @@
 from scope.models.cnn3d import get_cnn3d_model
 from scope.models.resnet3d import Resnet3DBuilder
-import scope.models.efficientnet_3D.tfkeras as efn
 
 
 def get_model(width:int, height:int, depth:int, n_channels:int, model_type:str='cnn3d',
@@ -14,6 +13,7 @@ def get_model(width:int, height:int, depth:int, n_channels:int, model_type:str='
             (width, height, depth, n_channels),
             1, regression=regression, reg_factor=weight_decay_coefficient)
     elif model_type == 'efficientnet3d':
+        import scope.models.efficientnet_3D.tfkeras as efn
         model = efn.EfficientNetB0(input_shape= (width, height, depth, n_channels),
                                    weights=None, classes=1, include_top=True, regression=regression)
     else:
