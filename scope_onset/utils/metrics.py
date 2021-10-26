700cf31244561eca3644fcd47c18f38977b20a8f
Julian Klug <tensu.wave@gmail.com>
2021-10-26 13:10:08 +0200

    Allow for sample weights for regression AUC

--- scope_onset/utils/metrics.py
+++ scope_onset/utils/metrics.py
@@ -6,7 +6,7 @@ class RegressionAUC(tf.keras.metrics.Metric):
         super(RegressionAUC, self).__init__(name=name, **kwargs)
         self.internal_model = tf.keras.metrics.AUC()
 
-    def update_state(self, y_true, y_pred):
+    def update_state(self, y_true, y_pred, sample_weight=None):
         # binarize using 4.5h threshold
         y_true = y_true / 60 > 4.5
         y_pred = y_pred / 60 > 4.5
@@ -14,7 +14,7 @@ class RegressionAUC(tf.keras.metrics.Metric):
         y_true = tf.cast(y_true, tf.bool)
         y_pred = tf.cast(y_pred, tf.bool)
 
-        self.internal_model.update_state(y_true, y_pred)
+        self.internal_model.update_state(y_true, y_pred, sample_weight)
 
     def result(self):
         return self.internal_model.result()
